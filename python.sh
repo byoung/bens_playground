c4ff5ae91329aa4a6499080f7f1fd26c16af1340
byoung <benjamintyoung@gmail.com>
2019-02-02 17:22:51 -0500

    Adapt (sort of) to the python environments bin folder on Windows.

--- python.sh
+++ python.sh
@@ -5,7 +5,15 @@
 # Credit to the site below for a simple way of setting up your env
 # https://anbasile.github.io/programming/2017/06/25/jupyter-venv/
 
+# Both
 python -m venv python_env
+
+# Linux
 source python_env/bin/activate
+
+# Windows
+source python_env/Scripts/activate
+
+# Both
 pip install ipykernel
 ipython kernel install --user --name=python_env
